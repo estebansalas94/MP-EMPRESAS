ee388e85f3f9695d386da8134c1a86b5a8a5f52e
Esteban David Salas Cortina <estebansalas0404@gmail.com>
2025-01-30 08:36:17 -0500

    Rutas y layouts en ReactJS

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LayoutPublic from './layouts/LayoutPublic';
+import LayoutAdmin from './layouts/LayoutAdmin';
+import LayoutClient from './layouts/LayoutClient';
+import Pagehome from './pagepublic/Pagehome';
+import ProtectedRoutes from './pageauth/ProtectedRoutes';
 
 function App() {
   return (
-    <>
-        MP - EMPRESAS
-        <button className='btn btn-primary'>Boton</button>
-    </>
+    <Router>
+      <Routes>
+
+        <Route path="/" element={<LayoutPublic/>}>
+          <Route index element={<Pagehome/>} />
+        </Route>
+
+        <Route element={<ProtectedRoutes/>}>
+          <Route  path="/admin" element={<LayoutAdmin/>}>
+            <Route  index element={<Pagehome/>}/>
+          </Route>
+          <Route  path="/client" element={<LayoutClient/>}>
+            <Route  index element={<Pagehome/>}/>
+          </Route>
+        </Route>
+        
+      </Routes>
+    </Router>
   )
 }
 
