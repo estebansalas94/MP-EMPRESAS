a47d01b2f7c0deeeaa564c0c4643bc03b751e5f7
Esteban David Salas Cortina <estebansalas0404@gmail.com>
2025-01-31 10:28:23 -0500

    Mostrar los usuarios clientes.

---
+++ resources/js/pageadmin/UserAll.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import Sidebar from './Sidebar';
+import Config from '../Config';
+
+const UserAll = () => {
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        getUserAll();
+    },[]);
+
+    const getUserAll = async () => {
+        try {
+            const response = await Config.getUserAll();
+            setUsers(response.data);
+        } catch (error) {
+            console.error("Error al obtener usuarios:", error.response?.data || error.message);
+        }
+    }
+  return (
+    <div className="container bg-light">
+        <div className='row'>
+            <Sidebar/>
+            <div className="col-sm-9 mt-3 mb-3">
+                <div className="card">
+                    <div className="card-body">
+                        <table className='table'>
+                            <thead>
+                                <tr>
+                                    <th>ORDEN</th><th>NOMBRE</th><th>ACCIÓN</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    !users ? "...Loading" : users.map(
+                                        (user) => {
+                                            return(
+                                                <tr key={user.id}>
+                                                    <td>{user.id}</td>
+                                                    <td>{user.name}</td>
+                                                    <td></td>
+                                                </tr>
+                                            )
+                                        }
+                                    )
+                                }
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UserAll
